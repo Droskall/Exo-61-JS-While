92e5285b4efc7e75d25f4fbbdfc6c3efab29d743
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-11-15 16:35:01 +0100

    finish

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,8 +1,9 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
 let i = 15;
-while (i > 0) {
-    document.getElementById('monDiv').innerHTML += "Ma variable i vaux"+ i + "<br>";
+while (i < 60  ) {
+    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i;
+    i += 15 ;
 }
 
 
